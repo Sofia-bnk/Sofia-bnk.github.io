7c14a964f059d857a797f8745794be900fd2860c
Sofia Bonakdar <sofia@bonakdar.se>
2021-02-02 20:20:49 +0100

    sending my project

---
+++ precache-manifest.b8260372724b40e3b4695597417a394a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "383cd31d8dd221cf4172",
+    "url": "/css/app.639c71a5.css"
+  },
+  {
+    "revision": "e6dc279720805bf7b5b6f36e5f7f0703",
+    "url": "/index.html"
+  },
+  {
+    "revision": "383cd31d8dd221cf4172",
+    "url": "/js/app.894c429f.js"
+  },
+  {
+    "revision": "d848f1a1abc21c3cf1c9",
+    "url": "/js/chunk-vendors.91813fa8.js"
+  },
+  {
+    "revision": "dd1eed2035da343167f6c381b96779c3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
